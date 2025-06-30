fd01a0c4139627189595c783e1868216ea8ee0ee
linnkhant07 <lkty07@gmail.com>
2025-06-30 14:46:45 -0700

    update lng

--- backend/utils/getCycle2Status.js
+++ backend/utils/getCycle2Status.js
@@ -3,7 +3,7 @@ export function getCycle2Status(lat, lon) {
 
   const isLikely = (
     (lat >= -85 && lat <= -35 && lon >= -180 && lon <= -150) ||     // Region 1
-    (lat >= -87 && lat <= 0 && lon >= -135 && lon <= 95) ||       // Region 2 
+    (lat >= -87 && lat <= 0 && lon >= -140 && lon <= 95) ||       // Region 2 
     (lat >= -85 && lat <= -40 && lon >= 155 && lon <= 180) ||     // Region 3
     (lat >= 0 && lat <= 15 && lon >= -45 && lon <= 30) ||         // Region 4
     (lat >= 0 && lat <= 75 && lon >= -120 && lon <= -55) ||        // Region 5
